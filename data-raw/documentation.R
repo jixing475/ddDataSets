76826eae6319580a7cc42414592e03211e01c528
jixing <jixing475@163.com>
2020-03-11 11:09:13 +0800

    Initial commit

---
+++ data-raw/documentation.R
@@ -0,0 +1,32 @@
+#' ddDataSets
+#' A data package for ddDataSets.
+#' @docType package
+#' @aliases ddDataSets-package
+#' @title Package Title
+#' @name ddDataSets
+#' @description A description of the data package
+#' @details Use \code{data(package='ddDataSets')$results[, 3]} to see a list of available data sets in this data package
+#'     and/or DataPackageR::load_all
+#' _datasets() to load them.
+#' @seealso
+#' \link{tidy_cars}
+NULL
+
+
+
+#' Detailed description of the data
+#' @name tidy_cars
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{spec_tbl_df} containing the following fields:
+#' \describe{
+#' \item{speed_mph}{}
+#' \item{stopping_distace}{}
+#' }
+#' @source The data comes from________________________.
+#' @seealso
+#' \link{ddDataSets}
+NULL
+
+
+
